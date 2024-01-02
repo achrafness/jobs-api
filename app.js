b4697784d0b64207c71cb4008d8a9cec4e70fb0d
achrafness <achraf.nessighaoui13@gmail.com>
2024-01-02 21:38:18 +0100

    Fix problems

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ const authenticateUser = require("./middleware/authentication")
 // router 
 const authRouter = require("./routes/auth")
 const jobsRouter = require("./routes/jobs")
-app.use("/",(req,res)=>{
-  res.send("achraf zin")
-})
+
 app.set('trust proxy', 1);
 app.use(
   rateLimiter({
@@ -35,6 +33,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 // routes
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/jobs", authenticateUser ,jobsRouter)
